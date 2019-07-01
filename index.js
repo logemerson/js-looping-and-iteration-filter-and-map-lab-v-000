b5616f76eb695e9d83ad3854294702d55eb7a615
Logan Emerson <logemerson@gmail.com>
2019-07-01 14:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(function(driver) {
-    return driver.revenue > revenue;
-  });
-}
-
-function driverNamesWithRevenueOver(drivers, revenue) {
-  let driverObjs = driversWithRevenueOver(drivers, revenue);
-  return driverObjs.map(function(driver) {
-    return driver.name;
-  });
-}
-
-function exactMatch(drivers, attributeObj) {
-  return drivers.filter(function(driver) {
-    for (let key in attributeObj) {
-      return driver[key] === attributeObj[key];
-    }
-  });
-}
-
-function exactMatchToList(drivers, attributeObj) {
-  let driverObjs = exactMatch(drivers, attributeObj);
-  return driverObjs.map(function(driver) {
-    return driver.name;
-  });
+function driversWithRevenueOver(driverObjs, revenue) {
+  debugger;
 }
