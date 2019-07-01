27b0fc235c787fe246186c63de4f32d72993aec0
Logan Emerson <logemerson@gmail.com>
2019-07-01 14:37:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function driversWithRevenueOver(driverObjs, revenue) {
   // driverObjs = [{name: "Sally", revenue: 400}, ...]
   const result = driverObjs.filter(function(driver) {
     return driver.revenue > revenue;
-    debugger;
   })
   return result;
 }
